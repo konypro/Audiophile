0ffe1116156d25abfda2501c590eeeadcf89bb90
konypro <ktyker.kt@gmail.com>
2022-11-21 16:46:42 +0100

    Add Artist utils

---
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistUtils.java
@@ -0,0 +1,14 @@
+package de.konstantintyker.audiophile.backend.artists;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ArtistUtils {
+    public String generateUUID() {
+
+        return UUID.randomUUID().toString();
+    }
+
+}
