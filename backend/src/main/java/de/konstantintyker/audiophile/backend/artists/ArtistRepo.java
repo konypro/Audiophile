2cb14c3f9b24a9fae126b9a6cebf7e08b3b516c1
konypro <ktyker.kt@gmail.com>
2022-11-21 15:37:35 +0100

    Add Artist Repo

---
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistRepo.java
@@ -0,0 +1,6 @@
+package de.konstantintyker.audiophile.backend.artists;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ArtistRepo extends MongoRepository <Artist, String>{
+}
