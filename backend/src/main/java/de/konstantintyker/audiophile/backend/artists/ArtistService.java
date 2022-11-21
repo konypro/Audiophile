9bb3eee10157f2ac88d5642daa24845bf9b097ab
konypro <ktyker.kt@gmail.com>
2022-11-21 16:06:47 +0100

    Add Artist service

---
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistService.java
@@ -0,0 +1,25 @@
+package de.konstantintyker.audiophile.backend.artists;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArtistService {
+    private final ArtistRepo artistRepo;
+    private final ArtistUtils artistUtils;
+
+    public ArtistService(ArtistRepo artistRepo, ArtistUtils artistUtils) {
+        this.artistRepo = artistRepo;
+        this.artistUtils = artistUtils;
+    }
+
+    public Artist addArtistDate(NewArtist newArtist) {
+        String uuid = this.artistUtils.generateUUID();
+        Artist artist = new Artist(newArtist.firstName(),newArtist.lastName(),uuid);
+        return this.artistRepo.save(artist);
+    }
+    public List<Artist> getArtistList(){
+        return this.artistRepo.findAll();
+    }
+}
