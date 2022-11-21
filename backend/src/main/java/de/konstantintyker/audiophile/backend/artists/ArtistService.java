c8d218647e5f66e6bebe50afab001f623f09778d
konypro <ktyker.kt@gmail.com>
2022-11-21 16:49:59 +0100

    Fix ArtistService List

--- backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistService.java
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistService.java
@@ -8,6 +8,10 @@ import java.util.List;
 public class ArtistService {
     private final ArtistRepo artistRepo;
     private final ArtistUtils artistUtils;
+    public List<Artist> getArtistList(){
+        return this.artistRepo.findAll();
+    }
+
 
     public ArtistService(ArtistRepo artistRepo, ArtistUtils artistUtils) {
         this.artistRepo = artistRepo;
@@ -18,8 +22,5 @@ public class ArtistService {
         String uuid = this.artistUtils.generateUUID();
         Artist artist = new Artist(newArtist.firstName(),newArtist.lastName(),uuid);
         return this.artistRepo.save(artist);
-    }
-    public List<Artist> getArtistList(){
-        return this.artistRepo.findAll();
     }
 }
