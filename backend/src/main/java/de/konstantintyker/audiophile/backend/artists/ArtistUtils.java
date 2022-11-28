7a7b24b09979a6c06e9c9f3108a158d843ad52d5
konypro <ktyker.kt@gmail.com>
2022-11-28 15:25:35 +0100

    Add empty contractor in util for sonar

--- backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistUtils.java
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistUtils.java
@@ -1,12 +1,8 @@
 package de.konstantintyker.audiophile.backend.artists;
-
 import java.util.UUID;
-
-
 public class ArtistUtils {
     private ArtistUtils() {
     }
-
     public static String generateUUID() {
         return UUID.randomUUID().toString();
     }
