01e439978904299be1f0d2bc3737eef502cbc0de
konypro <ktyker.kt@gmail.com>
2022-11-28 12:26:31 +0100

    Add postMapping in controller

--- backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistController.java
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistController.java
@@ -1,9 +1,8 @@
 package de.konstantintyker.audiophile.backend.artists;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,8 +16,14 @@ public class ArtistController {
     }
 
     @GetMapping
-    List<Artist> getArtistList(){
+    List<Artist> getArtistList() {
         return artistService.getArtistList();
     }
+
+    @PostMapping
+    public Artist addNewArtist(@Valid @RequestBody NewArtist artist) {
+        return artistService.addNewArtist(artist);
+    }
+
 
 }
