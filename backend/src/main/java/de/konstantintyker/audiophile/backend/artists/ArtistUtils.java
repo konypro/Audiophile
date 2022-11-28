5775ae6cde3376aa88713bc95490c17fa0cb4cc4
konypro <ktyker.kt@gmail.com>
2022-11-28 15:18:47 +0100

    Add util class

---
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/ArtistUtils.java
@@ -0,0 +1,14 @@
+package de.konstantintyker.audiophile.backend.artists;
+
+import java.util.UUID;
+
+
+public class ArtistUtils {
+    private ArtistUtils() {
+    }
+
+    public static String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+}
