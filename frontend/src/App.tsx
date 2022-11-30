13811b87de51254b9edb4a1476debbd8c5778b59
konypro <ktyker.kt@gmail.com>
2022-11-30 07:57:07 +0100

    Delete routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import {HashRouter, Route, Routes,} from "react-router-dom"
 import AllArtists from "./Artist/ArtistModel/AllArtists";
 
 export default function App() {
@@ -9,12 +8,7 @@ export default function App() {
     <header>
       <h1>AUDIOphile</h1>
     </header>
-    <HashRouter>
-      <Routes>
-        <Route path="/allArtists" element={<AllArtists/>}/>
-
-      </Routes>
-    </HashRouter>
+    <AllArtists/>
 
 
   </>
