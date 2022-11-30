5a764198ff13b842b7459877b77f2136d4ad9ece
konypro <ktyker.kt@gmail.com>
2022-11-30 08:06:35 +0100

    Add Artist card

--- backend/src/test/java/de/konstantintyker/audiophile/backend/artist/ArtistIntegrationTest.java
+++ backend/src/test/java/de/konstantintyker/audiophile/backend/artist/ArtistIntegrationTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -16,6 +18,7 @@ class ArtistIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
+
     @Test
     void WhenGetArtistsIsOk() throws Exception {
 
@@ -25,4 +28,20 @@ class ArtistIntegrationTest {
                 .andExpect(content().json("[]"));
 
     }
+
+    @Test
+    @DirtiesContext
+    void WhenAddArtist_Expect200() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/api/artists")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                        "firstName": "Elton",
+                                        "lastName": "John"     
+                                }
+                                """))
+                .andExpect(status().isOk());
+    }
 }
+
+
