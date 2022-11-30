69627c826ccca5232660b8543bbcd1737e7c30f7
konypro <ktyker.kt@gmail.com>
2022-11-30 17:04:17 +0100

    Add bootstrap fetchers

--- frontend/src/Artist/ArtistModel/ArtistCard.tsx
+++ frontend/src/Artist/ArtistModel/ArtistCard.tsx
@@ -1,12 +1,36 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
 import React from 'react';
 import {ArtistModel} from "./ArtistModel";
-type props ={artistCard: ArtistModel}
+
+type props = { artistCard: ArtistModel }
+
 function ArtistCard(props: props) {
     return (
-        <div>
-            <p>{props.artistCard.firstName}</p>
-            <p>{props.artistCard.lastName}</p>
-        </div>
+        <Card border="dark" style={{width: '18rem'}}>
+            <Card.Header>
+                <Nav variant="tabs" defaultActiveKey="#first">
+                    <Nav.Item>
+                        <Nav.Link href="#first">Artist</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">PlayList</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{props.artistCard.firstName + " " + props.artistCard.lastName}</Card.Title>
+
+                <Button variant="light">Delete</Button>
+            </Card.Body>
+        </Card>
+        // <p>{props.artistCard.firstName}</p>
+        //<p>{props.artistCard.lastName}</p>
+        //</div>
     );
 }
 
