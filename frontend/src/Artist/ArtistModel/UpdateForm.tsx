1da38932fbc04a5db32869a30d6b6f8a5dae4199
konypro <ktyker.kt@gmail.com>
2022-12-02 12:38:42 +0100

    Add update form

---
+++ frontend/src/Artist/ArtistModel/UpdateForm.tsx
@@ -0,0 +1,43 @@
+import {useState} from "react";
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import {ArtistModel} from "./ArtistModel";
+
+type AddArtistFormProps = {
+    artist: ArtistModel,
+    fetchAll: () => void
+}
+
+export default function AddArtistForm(props: AddArtistFormProps) {
+    const [firstName, setfirstName] = useState<String>(props.artist.firstName)
+    const [lastName, setlastName] = useState<String>(props.artist.lastName)
+
+
+    const updateNewArtist = () => {
+        axios.put("/api/artists/" + props.artist.id, {
+            firstName: firstName,
+            lastName: lastName,
+            id: props.artist.id
+        })
+            .catch((error) => console.log("UPDATE Error: " + error))
+            .then(props.fetchAll)
+
+    }
+    return (
+        <div id={"name"}>
+
+            <InputGroup size="sm" className="mb-3">
+                <Form.Control aria-label="Small" aria-describedby="inputGroup-sizing-sm" type={"text"}
+                              onChange={(event) => setfirstName(event.target.value)}
+                              placeholder={"first"}/>
+                <Form.Control aria-label="Small" aria-describedby="inputGroup-sizing-sm" type={"text"}
+                              onChange={(event) => setlastName(event.target.value)}
+                              placeholder={"last name"}/>
+            </InputGroup>
+            <Button variant="light" type={"submit"} onClick={updateNewArtist}>Save</Button>
+
+        </div>)
+
+}
