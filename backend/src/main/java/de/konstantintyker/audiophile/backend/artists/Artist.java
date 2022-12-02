e8d418a75c63a046bb321a7c0c1ddb968c76e931
konypro <ktyker.kt@gmail.com>
2022-12-02 11:08:28 +0100

    Add @with in Model

--- backend/src/main/java/de/konstantintyker/audiophile/backend/artists/Artist.java
+++ backend/src/main/java/de/konstantintyker/audiophile/backend/artists/Artist.java
@@ -1,8 +1,10 @@
 package de.konstantintyker.audiophile.backend.artists;
 
 
+import lombok.With;
 import org.springframework.data.annotation.Id;
 
+@With
 public record Artist(
         @Id
         String id,
