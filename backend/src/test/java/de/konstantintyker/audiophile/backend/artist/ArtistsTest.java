e7bea80be9da4fca1e8f0f22c38344d9322b5834
konypro <ktyker.kt@gmail.com>
2022-12-08 11:54:45 +0100

    Add ArtistUnitTest

--- backend/src/test/java/de/konstantintyker/audiophile/backend/artist/ArtistsTest.java
+++ backend/src/test/java/de/konstantintyker/audiophile/backend/artist/ArtistsTest.java
@@ -4,15 +4,14 @@ import de.konstantintyker.audiophile.backend.artists.Artist;
 import de.konstantintyker.audiophile.backend.artists.ArtistRepo;
 import de.konstantintyker.audiophile.backend.artists.ArtistService;
 import de.konstantintyker.audiophile.backend.artists.ArtistUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-class TravelerServiceTest {
+class ArtistServiceTest {
     ArtistRepo artistRepo = mock(ArtistRepo.class);
     ArtistUtils artistUtils = mock(ArtistUtils.class);
     ArtistService artistService = new ArtistService(artistRepo, artistUtils);
@@ -21,10 +20,33 @@ class TravelerServiceTest {
     void GetArtistsTest() {
         Artist artist = new Artist("123", "Elton", "John");
         List<Artist> artists = List.of(artist);
-
         when(artistRepo.findAll()).thenReturn(artists);
         List<Artist> actual = artistService.getArtistList();
-        Assertions.assertEquals(artists, actual);
+        assertEquals(artists, actual);
+    }
+
+    @Test
+    void AddNewArtistWithId() {
+        Artist artist = new Artist("123", "Elton", "John");
+        when(artistRepo.save(artist)).thenReturn(artist);
+        when(artistUtils.generateUUID()).thenReturn("123");
+
+        Artist result = artistService.addNewArtist(artist);
+        verify(artistRepo).save(artist);
+        assertEquals(artist, result);
+    }
+
+    @Test
+    void updateArtistById() {
+
+
+        Artist updatedArtist = new Artist("123", "Elton", "John");
+
+        when(artistRepo.existsById("123")).thenReturn(true);
+        when(artistRepo.save(updatedArtist)).thenReturn(updatedArtist);
+        Artist actual = artistService.updateArtistById("123", updatedArtist);
+
+        assertEquals(updatedArtist, actual);
     }
 
 
