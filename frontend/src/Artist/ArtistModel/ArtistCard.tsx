847763a8c16e7fdcfafcb5f718742af2def00f9e
konypro <ktyker.kt@gmail.com>
2022-12-13 16:19:56 +0100

    Add new styling for artistCard

--- frontend/src/Artist/ArtistModel/ArtistCard.tsx
+++ frontend/src/Artist/ArtistModel/ArtistCard.tsx
@@ -1,11 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Nav from 'react-bootstrap/Nav';
 import React, {useState} from 'react';
 import {ArtistModel} from "./ArtistModel";
 import UpdateForm from "./UpdateForm";
 import axios from "axios";
 import {GoTrashcan} from "react-icons/go";
+import Table from 'react-bootstrap/Table';
 
 type props = {
     artistCard: ArtistModel,
@@ -23,20 +23,23 @@ function ArtistCard(props: props) {
     }
     return (
         <Card border="dark" style={{width: '22rem'}}>
-            <Card.Header>
-                <Nav justify variant="tabs" defaultActiveKey="#first">
-                    <Nav.Item>
-                        <Nav.Link href="#first">Artist</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Card.Header>
+            <Card.Header>Artist</Card.Header>
             <Card.Body>
                 <Card.Title>
-                    <ul>
-                        <li> {props.artistCard.firstName + "  " + props.artistCard.lastName}</li>
-                        <br/>
-                        <li><a href={props.artistCard.url}> {props.artistCard.url}</a></li>
-                    </ul>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>{props.artistCard.firstName + "  " + props.artistCard.lastName}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>official</td>
+                            <td><Card.Link href={props.artistCard.url}>{props.artistCard.url}</Card.Link></td>
+                        </tr>
+                        </tbody>
+                    </Table>
                 </Card.Title>
                 {edit && <UpdateForm artist={props.artistCard} deleteArtist={deleteArtist} setEdit={setEdit}
                                      fetchAll={props.fetchAll}/>}
